97b57c3a3349f2e23a847c4cb0cf16c6b7a02738
Sagar Talagatti <sagartalagatti@gmail.com>
2023-05-24 21:15:12 +0530

    Completed the project

--- src/components/UI/Input.js
+++ src/components/UI/Input.js
@@ -1,13 +1,15 @@
+import React from "react";
+
 import classes from "./Input.module.css";
 
-const Input = (props) => {
+const Input = React.forwardRef((props, ref) => {
   return (
     <div className={classes.input}>
       <label htmlFor={props.input.id}>{props.label}</label>
-      <input {...props.input} />
+      <input ref={ref} {...props.input} />
       {/* this adds all properties of props.input as key=value pairs for the input tag */}
     </div>
   );
-};
+});
 
 export default Input;
